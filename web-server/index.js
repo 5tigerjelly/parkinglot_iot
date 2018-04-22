e52d3b9716ea81ce2847a2a52e56b5658de92019
Bum Mook Oh <bmo5@uw.edu>
2018-04-22 09:16:55 -0700

    fix

--- web-server/index.js
+++ web-server/index.js
@@ -139,7 +139,7 @@ function getLocationByLicense(license) {
 // the pi will make a HTTP POST request to this server, update the 
 function updateParkingSpace(lotID, floor, spaceID, isOccupied) {
     db.lot[lotID][floor][spaceID]['isOccupied'] = isOccupied;
-    if (isOccupied == true) {
+    if (isOccupied == 'True') {
         db.lot[lotID][floor][`emptySpace`]--;
         db.lot[lotID][`emptySpace`]--;
     } else {
