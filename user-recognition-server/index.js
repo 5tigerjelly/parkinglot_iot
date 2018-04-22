2295d5311b78e8369d437f6caedd000210a4eeb5
Wesley Wei <wesley631w@gmail.com>
2018-04-22 00:02:07 -0700

    Added create, get and delete FaceList for face API.

--- user-recognition-server/index.js
+++ user-recognition-server/index.js
@@ -4,14 +4,79 @@ var URI_BASE = "https://westus.api.cognitive.microsoft.com/face/v1.0/facelists/"
 
 // Gets new employee information to make a new employee
 function createNewEmployee() {
+    var userName = document.getElementById("userId").value;
     var firstName = document.getElementById("firstName").value;
     var lastName = document.getElementById("lastName").value;
     var licenseNum = document.getElementById("licenseNum").value;
-    var employeeArray = [firstName, lastName, licenseNum];
-    console.log(employeeArray);
-    
-    jquery.ajax({
-        
+
+    $.ajax({
+        url: URI_BASE + userName,
+        beforeSend: function(xhrObj){
+            // Request headers
+            xhrObj.setRequestHeader("Content-Type","application/json");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", API_KEY);
+        },
+        type: "PUT",
+        // Request body
+        data: JSON.stringify({ 
+            "name": firstName + lastName,
+            "userData": licenseNum
+        }),
+    })
+    .done(function(data) {
+        alert("success");
+    })
+    .fail(function() {
+        alert("error");
     });
 }
 
+// Gets new employee information to make a new employee
+function getEmployeeData() {
+    var getUserName = document.getElementById("deleteUserId").value;
+
+    $.ajax({
+        url: URI_BASE + getUserName,
+        beforeSend: function(xhrObj){
+            // Request headers
+            xhrObj.setRequestHeader("Content-Type","application/json");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", API_KEY);
+        },
+        type: "GET",
+        // Request body
+        data: {},
+    })
+    .done(function(data) {
+        alert("success");
+        console.log(data);
+        for (var users in userList) {
+            console.log(users);
+        }
+    })
+    .fail(function() {
+        alert("Username not found");
+    });
+}
+
+// Deletes new employee information to make a new employee
+function deleteEmployee() {
+    var deleteUserName = document.getElementById("deleteUserId").value;
+
+    $.ajax({
+        url: URI_BASE + deleteUserName,
+        beforeSend: function(xhrObj){
+            // Request headers
+            xhrObj.setRequestHeader("Content-Type","application/json");
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", API_KEY);
+        },
+        type: "DELETE",
+        // Request body
+        data: {},
+    })
+    .done(function(data) {
+        alert("success");
+    })
+    .fail(function() {
+        alert("error");
+    });
+}
